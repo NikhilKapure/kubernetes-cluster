ff443647b3699f51009e15d0a1ffd5884b50163c
Nikhil Kapure <Nkapure11@gmail.com>
2017-08-04 20:33:02 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #=======================================================================================================================
 
-if (node['platform'] == "centos") & (node['platform_version'] >= "7") & (node['kubernetes-cluster']['k8s-version'] == "1.7") & (node['kubernetes-cluster']['agent'] == "master")
+if (node['platform_family'] == "rhel") & (node['platform_version'] >= "7") & (node['kubernetes-cluster']['k8s-version'] == "1.7") & (node['kubernetes-cluster']['agent'] == "master")
   include_recipe 'kubernetes-cluster::kube_master_centos_1.7'
-  elsif (node['platform'] == "centos") & (node['platform_version'] >= "7") & (node['kubernetes-cluster']['k8s-version'] == "1.7") & (node['kubernetes-cluster']['agent'] == "minion")
+  elsif (node['platform_family'] == "rhel") & (node['platform_version'] >= "7") & (node['kubernetes-cluster']['k8s-version'] == "1.7") & (node['kubernetes-cluster']['agent'] == "minion")
   include_recipe 'kubernetes-cluster::kube_minion_centos_1.7'
   else
   Chef::Log.info('OS Compatibility issue.')
