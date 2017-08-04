3386d50937eebe96127a3d4197314c8c7401b16a
Nikhil Kapure <Nkapure11@gmail.com>
2017-08-04 18:20:40 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nikhil.kapure@reancloud.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Kubernetes-Cluster'
 long_description 'Installs/Configures Kubernetes-Cluster'
-version '0.1.0'
+version '0.1.3'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports %w(debian rhel)
 
